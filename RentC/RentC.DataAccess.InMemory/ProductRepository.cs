503b1f342406c34a480c6d7a30c75657d49b1789
FeverGrayVan <grayvanhq@gmail.com>
2020-01-18 16:20:18 +0200

    Product Manager Pages Built and Tested

--- RentC/RentC.DataAccess.InMemory/ProductRepository.cs
+++ RentC/RentC.DataAccess.InMemory/ProductRepository.cs
@@ -19,7 +19,7 @@ namespace RentC.DataAccess.InMemory
 
             if (products == null)
             {
-                products = new List<Product>;
+                products = new List<Product>();
 
             }
         }
