f40718fae9251d490a0c919d831517df0b9160cd
FeverGrayVan <grayvanhq@gmail.com>
2020-01-18 18:25:42 +0200

    Category Dropdown added and tested

--- RentC/RentC.WebUI/Controllers/ProductManagerController.cs
+++ RentC/RentC.WebUI/Controllers/ProductManagerController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using RentC.Core.Models;
+using RentC.Core.ViewModels;
 using RentC.DataAccess.InMemory;
 
 namespace RentC.WebUI.Controllers
@@ -11,10 +12,12 @@ namespace RentC.WebUI.Controllers
     public class ProductManagerController : Controller
     {
         ProductRepository context;
+        ProductCategoryRepository productCategories;
 
         public ProductManagerController()
         {
             context = new ProductRepository();
+            productCategories = new ProductCategoryRepository();
         }
         // GET: ProductManager
         public ActionResult Index()
@@ -25,8 +28,11 @@ namespace RentC.WebUI.Controllers
 
         public ActionResult Create()
         {
-            Product product = new Product();
-            return View(product);
+            ProductManagerViewModel viewModel = new ProductManagerViewModel();
+
+            viewModel.Product = new Product();
+            viewModel.ProductCategories = productCategories.Collection();
+            return View(viewModel);
         }
         [HttpPost]
         public ActionResult Create(Product product)
@@ -54,7 +60,10 @@ namespace RentC.WebUI.Controllers
             }
             else
             {
-                return View(product);
+                ProductManagerViewModel viewModel = new ProductManagerViewModel();
+                viewModel.Product = product;
+                viewModel.ProductCategories = productCategories.Collection();
+                return View(viewModel);
             }
         }
         [HttpPost]
